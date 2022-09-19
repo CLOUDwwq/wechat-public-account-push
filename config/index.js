333ab32dbd0b2168383096c00eb73a0f4fb42628
CLOUDwwq <113869269+CLOUDwwq@users.noreply.github.com>
2022-09-19 11:17:43 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ export const config = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "90a4d4eaa11b6491734c0ffc20327be2",
+  APP_SECRET: "b449e1b705b2cfdd05706c9b85e572c8",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
